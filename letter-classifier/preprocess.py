c14f7ec483933ba383058a69f2420278838998e2
Karlo Čeh <karlo.ceh@fer.hr>
2020-03-29 20:22:49 +0200

    preprocessing improved

--- letter-classifier/preprocess.py
+++ letter-classifier/preprocess.py
@@ -37,26 +37,38 @@ if __name__ == "__main__":
     print(file)
 
     matplotlib.interactive(True)
-    im = cv2.imread(file)
-    grayimg = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
+    image = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
     plt.figure(figsize=(8, 8))
-    plt.imshow(grayimg, cmap='gray')
+    plt.imshow(image, cmap='gray')
     input('press return to continue')
+    #cv2.imshow("Gray", grayimg)
+    #cv2.waitKey(0)
 
-    contrast_fixed = cv2.convertScaleAbs(grayimg, alpha=2.5, beta=0)
-    plt.figure(figsize=(8, 8))
-    plt.imshow(contrast_fixed)
+    '''
+    image_enhanced = cv2.equalizeHist(image)
+    plt.imshow(image_enhanced, cmap='gray')
     input('press return to continue')
+    contrast_fixed = cv2.convertScaleAbs(image, alpha=2.5, beta=50)
+    plt.figure(figsize=(8, 8))
+    plt.imshow(contrast_fixed, cmap='gray')
+    input('press return to continue')
+    #cv2.imshow("Contrast", contrast_fixed)
+    #cv2.waitKey(0)
+    '''
     
-    _, binary_thresh = cv2.threshold(contrast_fixed, 200, 255, cv2.THRESH_BINARY_INV)
+    #_, th = cv2.threshold(image, 200, 255, cv2.THRESH_BINARY_INV)
+    th=cv2.adaptiveThreshold(image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+    cv2.imwrite(file+"-result.png", th)
     fig = plt.figure(figsize=(8, 8))
     fig.add_subplot(1, 2, 1)
-    plt.imshow(contrast_fixed, cmap='gray')
+    plt.imshow(image, cmap='gray')
     fig.add_subplot(1, 2, 2)
-    plt.imshow(binary_thresh, cmap='gray')
+    plt.imshow(th, cmap='gray')
     input('press return to continue')
+    #cv2.imshow("Binarization", binary_thresh)
+    #cv2.waitKey(0)
 
-    lines = cv2.HoughLinesP(binary_thresh, 1, np.pi/180, 100, 
+    lines = cv2.HoughLinesP(th, 1, np.pi/180, 100, 
                             minLineLength= 600/2.0, maxLineGap=20)
     angle = 0
     for line in lines:
