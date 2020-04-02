4ae51e759ce09c790fdd37e11c47a40b22df2c1a
Karlo Čeh <karlo.ceh@fer.hr>
2020-04-02 21:40:42 +0200

    added test for increasing contrast

--- letter-classifier/contrast-increase.py
+++ letter-classifier/contrast-increase.py
@@ -1,19 +1,19 @@
-import numpy as np
-import cv2
+from PIL import Image, ImageEnhance
 import os
 
 path = os.path.dirname(os.path.abspath(__file__))
 dir_for_test_samples=dir_for_results=os.path.join(path, "test-slike")
-dir_for_results=os.path.join(path, "contrast-normalization-results")
-print(dir_for_results)
+dir_for_results=os.path.join(path, "contrast-increase-results")
 
 for (dirpath, dirnames, filenames) in os.walk(dir_for_test_samples):
-        print(filenames)
         for filename in filenames:
             image_path=os.path.join(dir_for_test_samples, filename)
-            img = cv2.imread(image_path,0)
-            clahe = cv2.createCLAHE(tileGridSize=(8,8))
-            cl1 = clahe.apply(img)
+            im = Image.open(image_path).convert("L")
+            enhancer = ImageEnhance.Contrast(im)
+
             splited_filename=os.path.splitext(filename)
             path_to_save=os.path.join(dir_for_results, splited_filename[0]+'-result.png')
-            cv2.imwrite(path_to_save,cl1)
+
+            factor = 1.8
+            im_output = enhancer.enhance(factor)
+            im_output.save(path_to_save)
