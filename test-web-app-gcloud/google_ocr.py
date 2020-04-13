4f2552fc0d5b13bd0ef33bd5bce3cceb33f21727
Karlo Čeh <karlo.ceh@fer.hr>
2020-04-13 16:30:31 +0200

    repository change

---
+++ test-web-app-gcloud/google_ocr.py
@@ -0,0 +1,27 @@
+from google.cloud import vision
+import io
+import os
+
+path = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER=os.path.join(path, "static/uploads/")
+
+def init():
+    #if env var not set use:
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r"text-recognition-273618-655720c6137b.json"
+    client_options = {'api_endpoint': 'eu-vision.googleapis.com'} #use EU endpoint
+    client = vision.ImageAnnotatorClient(client_options=client_options)
+    return client
+
+def ocr(client, filename):
+    file=os.path.join(UPLOAD_FOLDER, filename)
+    with io.open(file, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.types.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print('Texts:')
+
+    output=texts[0].description
+    return output
