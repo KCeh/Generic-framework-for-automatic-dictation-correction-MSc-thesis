6e4c3aef46a854f3c756fb9980e4ed50a1ae5a20
Karlo Čeh <karlo.ceh@fer.hr>
2020-04-24 16:19:44 +0200

    updated constants

--- web-app/generic-framework-for-automatic-dictation-correction/src/main/java/hr/fer/genericframeworkforautomaticdictationcorrection/Utils/Constants.java
+++ web-app/generic-framework-for-automatic-dictation-correction/src/main/java/hr/fer/genericframeworkforautomaticdictationcorrection/Utils/Constants.java
@@ -9,6 +9,9 @@ public interface Constants {
         String LOGIN ="/login";
         String LOGIN_WITH_PARMS = "/login*";
         String LOGOUT="/logout";
+        String REGISTRATION="/signup";
+        String REGISTRATION_COFIRMATION = "/registrationConfirm*";
+        String BAD_USER="/badUser";
 
         String RESOURCES = "/resources/**";
         String JS = "/js/**";
@@ -18,10 +21,17 @@ public interface Constants {
 
     interface Redirect{
         String BASE_PATH="redirect:/";
+        String LOGIN="redirect:/login";
+        String BAD_USER = "redirect:/badUser";
+        String LOGIN_CONFIRMED_ACC="redirect:/login?message=Account+confirmed";
     }
 
     interface Views{
         String INDEX = "index";
         String LOGIN = "loginForm";
+        String REGISTRATION ="registration";
+        String REGISTRATION_SUCCESS="successfulRegistration";
+        String EMAIL_ERROR="emailError";
+        String BAD_USER="badUser";
     }
 }
