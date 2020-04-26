0dd8888406b98437667a42c8631a3b40103bfcd8
Karlo Čeh <karlo.ceh@fer.hr>
2020-04-26 13:45:57 +0200

    constants update for manage users

--- web-app/generic-framework-for-automatic-dictation-correction/src/main/java/hr/fer/genericframeworkforautomaticdictationcorrection/Utils/Constants.java
+++ web-app/generic-framework-for-automatic-dictation-correction/src/main/java/hr/fer/genericframeworkforautomaticdictationcorrection/Utils/Constants.java
@@ -18,6 +18,10 @@ public interface Constants {
         String SAVE_PASSWORD = "/user/savePassword*";
         String USER_UPDATE_PASSWORD = "/user/updatePassword*";
 
+        String MANAGE_USERS="/manageUsers";
+        String PROMOTE_USER="/manageUsers/promote";
+        String DEMOTE_USER="/manageUsers/demote";
+
         String RESOURCES = "/resources/**";
         String JS = "/js/**";
         String CSS = "/css/**";
@@ -26,6 +30,7 @@ public interface Constants {
 
     interface Redirect{
         String BASE_PATH="redirect:/";
+        String BASE_PATH_ACCESS_DENIED="redirect:/?message=Access+denied";
         String LOGIN="redirect:/login";
         String BAD_USER = "redirect:/badUser";
         String LOGIN_CONFIRMED_ACC="redirect:/login?message=Account+confirmed";
@@ -42,10 +47,16 @@ public interface Constants {
         String BAD_USER="badUser";
         String FORGOT_PASSWORD="forgotPassword";
         String UPDATE_PASSWORD="updatePassword";
-
+        String MANAGE_USERS="manageUsers";
     }
 
     interface Authority{
         String CHANGE_PASSWORD="CHANGE_PASSWORD_PRIVILEGE";
+    }
+
+    interface RoleNames{
+        String ADMIN="ROLE_ADMIN";
+        String TEACHER="ROLE_TEACHER";
+        String USER="ROLE_USER";
     }
 }
