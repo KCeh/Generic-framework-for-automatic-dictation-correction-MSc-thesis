8f89ccfbb0e9ef1528dee6fb2f5c0c3bcfe42c29
Karlo Čeh <karlo.ceh@fer.hr>
2020-04-26 13:44:33 +0200

    backend update for promote/demote

--- web-app/generic-framework-for-automatic-dictation-correction/src/main/java/hr/fer/genericframeworkforautomaticdictationcorrection/Controllers/AdminController.java
+++ web-app/generic-framework-for-automatic-dictation-correction/src/main/java/hr/fer/genericframeworkforautomaticdictationcorrection/Controllers/AdminController.java
@@ -1,9 +1,13 @@
 package hr.fer.genericframeworkforautomaticdictationcorrection.Controllers;
 
+import hr.fer.genericframeworkforautomaticdictationcorrection.Models.ManageUsersPostModel;
+import hr.fer.genericframeworkforautomaticdictationcorrection.Models.Role;
 import hr.fer.genericframeworkforautomaticdictationcorrection.Models.User;
 import hr.fer.genericframeworkforautomaticdictationcorrection.Models.UserViewModel;
+import hr.fer.genericframeworkforautomaticdictationcorrection.Services.RoleService;
 import hr.fer.genericframeworkforautomaticdictationcorrection.Services.UserService;
 import hr.fer.genericframeworkforautomaticdictationcorrection.Utils.Constants;
+import hr.fer.genericframeworkforautomaticdictationcorrection.Utils.GenericResponse;
 import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
@@ -11,9 +15,11 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestWrapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,6 +28,9 @@ public class AdminController {
 
     @Autowired
     UserService userService;
+
+    @Autowired
+    RoleService roleService;
 
     @RequestMapping(Constants.Paths.MANAGE_USERS)
     public String manage(SecurityContextHolderAwareRequestWrapper requestWrapper, Model model, final HttpServletRequest request){
@@ -46,5 +55,60 @@ public class AdminController {
         }
     }
 
+    @RequestMapping(value=Constants.Paths.PROMOTE_USER, method = RequestMethod.POST)
+    @ResponseBody
+    public GenericResponse promote(@RequestParam("email") String email){
+        User user = userService.findByEmail(email);
+
+        if(!ObjectUtils.isEmpty(user)){
+            Collection<Role> roles = user.getRoles();
+
+            if(roles.size()==3){
+                return new GenericResponse("Error","User+can't+be+promoted+further");
+            }else {
+                if(roles.size()==1){
+                    Role role = roleService.findByName("ROLE_TEACHER");
+                    roles.add(role);
+                }else if(roles.size()==2){
+                    Role role = roleService.findByName("ROLE_ADMIN");
+                    roles.add(role);
+                }
+                Collection<Role> sorted = roles.stream().sorted(Comparator.comparingLong(Role::getId)).collect(Collectors.toList());
+                user.setRoles(sorted);
+                userService.updateUser(user);
+            }
+        }else {
+            return new GenericResponse("Error","User+can't+be+found");
+        }
+        return new GenericResponse("Success");
+    }
+
+    @RequestMapping(value=Constants.Paths.DEMOTE_USER, method = RequestMethod.POST)
+    @ResponseBody
+    public GenericResponse demote(@RequestParam("email") String email){
+        User user = userService.findByEmail(email);
+
+        if(!ObjectUtils.isEmpty(user)){
+            Collection<Role> roles = user.getRoles();
+
+            if(roles.size()==1){
+                return new GenericResponse("Error","User+can't+be+demoted+further");
+            }else {
+                if(roles.size()==2){
+                    Role role = roleService.findByName("ROLE_TEACHER");
+                    roles.remove(role);
+                }else if(roles.size()==3){
+                    Role role = roleService.findByName("ROLE_ADMIN");
+                    roles.remove(role);
+                }
+                Collection<Role> sorted = roles.stream().sorted(Comparator.comparingLong(Role::getId)).collect(Collectors.toList());
+                user.setRoles(sorted);
+                userService.updateUser(user);
+            }
+        }else {
+            return new GenericResponse("Error","User+can't+be+found");
+        }
+        return new GenericResponse("Success");
+    }
 
 }
