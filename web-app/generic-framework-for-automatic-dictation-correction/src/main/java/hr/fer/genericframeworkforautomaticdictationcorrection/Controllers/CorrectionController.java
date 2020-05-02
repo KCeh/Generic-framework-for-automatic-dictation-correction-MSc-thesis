778f04ad587236852adc56f10b1ac96eeea673f0
Karlo Čeh <karlo.ceh@fer.hr>
2020-05-02 18:47:43 +0200

    create new correction backend added

--- web-app/generic-framework-for-automatic-dictation-correction/src/main/java/hr/fer/genericframeworkforautomaticdictationcorrection/Controllers/CorrectionController.java
+++ web-app/generic-framework-for-automatic-dictation-correction/src/main/java/hr/fer/genericframeworkforautomaticdictationcorrection/Controllers/CorrectionController.java
@@ -1,19 +1,26 @@
 package hr.fer.genericframeworkforautomaticdictationcorrection.Controllers;
 
+import hr.fer.genericframeworkforautomaticdictationcorrection.Exceptions.UserNotFoundException;
 import hr.fer.genericframeworkforautomaticdictationcorrection.Forms.NewCorrectionForm;
 import hr.fer.genericframeworkforautomaticdictationcorrection.Forms.NewDictateFrom;
 import hr.fer.genericframeworkforautomaticdictationcorrection.Models.*;
+import hr.fer.genericframeworkforautomaticdictationcorrection.OCR.OCR;
+import hr.fer.genericframeworkforautomaticdictationcorrection.OCR.OCRStrategyFactory;
 import hr.fer.genericframeworkforautomaticdictationcorrection.Services.*;
 import hr.fer.genericframeworkforautomaticdictationcorrection.Utils.Constants;
+import hr.fer.genericframeworkforautomaticdictationcorrection.Utils.GenericResponse;
 import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -37,6 +44,12 @@ public class CorrectionController {
     @Autowired
     RoleService roleService;
 
+    @Autowired
+    OCRStrategyFactory strategyFactory;
+
+    private Role teacher;
+
+
     @RequestMapping(Constants.Paths.CORRECTION_MY)
     public String showMyCorrections(Model model){
         org.springframework.security.core.userdetails.User userDetails = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
@@ -58,7 +71,7 @@ public class CorrectionController {
 
         NewCorrectionForm newCorrectionForm = new NewCorrectionForm(correctedDictation);
         model.addAttribute("correction", newCorrectionForm);
-        return Constants.Views.VIEW_DICTATE;
+        return Constants.Views.VIEW_CORRECTIONS;
     }
 
     @RequestMapping(value = Constants.Paths.CORRECTION_CREATE, method = RequestMethod.GET)
@@ -69,7 +82,63 @@ public class CorrectionController {
         org.springframework.security.core.userdetails.User userDetails = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         User user = userService.findByEmail(userDetails.getUsername());
 
-        Role teacher = roleService.findByName("ROLE_TEACHER");
+        List<Dictate> dictates = getDictates(user);
+        model.addAttribute("dictates", dictates);
+
+        List<String> OCRMethods = strategyFactory.getAllMethodesNames();
+        model.addAttribute("OCRMethods", OCRMethods);
+
+        return Constants.Views.CREATE_CORRECTION;
+    }
+
+    @RequestMapping(value = Constants.Paths.CORRECTION_CREATE, method = RequestMethod.POST)
+    public String createDictate(@ModelAttribute("correction") @Valid NewCorrectionForm correctionDto, BindingResult result, Model model, Errors errors) {
+        org.springframework.security.core.userdetails.User userDetails = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = userService.findByEmail(userDetails.getUsername());
+
+        if (result.hasErrors()) {
+            model.addAttribute("correction", correctionDto);
+            List<Dictate> dictates = getDictates(user);
+            model.addAttribute("dictates", dictates);
+            List<String> OCRMethods = strategyFactory.getAllMethodesNames();
+            model.addAttribute("OCRMethods", OCRMethods);
+            return Constants.Views.CREATE_DICTATE;
+        }
+
+        try {
+            if (ObjectUtils.isEmpty(user)) {
+                throw new UserNotFoundException("User not found");
+            }
+            OCR ocr = strategyFactory.getMethod(correctionDto.getUsedOCRMethod());
+            if(ObjectUtils.isEmpty(ocr)){
+                System.err.println("OCR is null");
+                throw new Exception("OCR is null");
+            }
+            String detectedText = ocr.detectText(correctionDto.getUrlOriginalImage());
+            correctionDto.setDetectedText(detectedText);
+            //draw on image
+            //call storage service for corrected image
+            //set url...
+            correctedDictationService.addCorrection(correctionDto, user);
+        } catch (Throwable ex) {
+            model.addAttribute("correction", correctionDto);
+            List<Dictate> dictates = getDictates(user);
+            model.addAttribute("dictates", dictates);
+            List<String> OCRMethods = strategyFactory.getAllMethodesNames();
+            model.addAttribute("OCRMethods", OCRMethods);
+            return Constants.Views.CREATE_DICTATE;
+        }
+
+        return Constants.Redirect.CORRECTION_MY;
+    }
+
+    private List<Dictate> getDictates(User user){
+        if(ObjectUtils.isEmpty(user)){
+            return new ArrayList<>();
+        }
+
+        teacher = roleService.findByName("ROLE_TEACHER");
+
         Collection<Role> roles = user.getRoles();
         List<Dictate> dictates;
         if(roles.contains(teacher)){
@@ -78,7 +147,21 @@ public class CorrectionController {
             dictates=dictateService.findByUser(user);
         }
 
-        model.addAttribute("dictates", dictates);
-        return Constants.Views.CREATE_DICTATE;
+        return dictates;
+    }
+
+    @RequestMapping(value = Constants.Paths.CORRECTION_UPLOAD_IMAGE, method = RequestMethod.POST)
+    @ResponseBody
+    public GenericResponse uploadFile(@RequestParam("file") MultipartFile file) {
+        String url;
+        try {
+            url = storageService.uploadImage(file);
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            return new GenericResponse("Error","Error while uploading file");
+        }
+
+        return new GenericResponse(url);
     }
 }
